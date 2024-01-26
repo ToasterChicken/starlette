eb5ad20953c682b2a861f07197cb13a4bf07861e
James Scott <jamie.scott@gmail.com>
2024-01-26 13:06:37 -0500

    First commit of redo of the sessoin persistents

--- starlette/middleware/sessions.py
+++ starlette/middleware/sessions.py
@@ -1,13 +1,52 @@
 import json
 import typing
 from base64 import b64decode, b64encode
+from datetime import datetime, timedelta, timezone
 
 import itsdangerous
-from itsdangerous.exc import BadSignature
+from itsdangerous.exc import BadSignature, SignatureExpired
 
 from starlette.datastructures import MutableHeaders, Secret
 from starlette.requests import HTTPConnection
 from starlette.types import ASGIApp, Message, Receive, Scope, Send
+
+# mutable mapping that keeps track of whether it has been modified
+class ModifiedDict(dict):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.modify = False
+        self.invalid = False
+
+    def __setitem__(self, key, value):
+        super().__setitem__(key, value)
+        self.modify = True
+
+    def __delitem__(self, key):
+        super().__delitem__(key)
+        self.modify = True
+
+    def clear(self):
+        super().clear()
+        self.modify = True
+
+    def pop(self, key, default=None):
+        value = super().pop(key, default)
+        self.modify = True
+        return value
+
+    def popitem(self):
+        value = super().popitem()
+        self.modify = True
+        return value
+
+    def setdefault(self, key, default=None):
+        value = super().setdefault(key, default)
+        self.modify = True
+        return value
+
+    def update(self, *args, **kwargs):
+        super().update(*args, **kwargs)
+        self.modify = True
 
 
 class SessionMiddleware:
@@ -20,6 +59,7 @@ class SessionMiddleware:
         path: str = "/",
         same_site: typing.Literal["lax", "strict", "none"] = "lax",
         https_only: bool = False,
+        refresh_window: typing.Optional[int] = None,
         domain: typing.Optional[str] = None,
     ) -> None:
         self.app = app
@@ -28,10 +68,33 @@ class SessionMiddleware:
         self.max_age = max_age
         self.path = path
         self.security_flags = "httponly; samesite=" + same_site
+        self.refresh_window = refresh_window
         if https_only:  # Secure flag can be used with HTTPS only
             self.security_flags += "; secure"
         if domain is not None:
             self.security_flags += f"; domain={domain}"
+
+    #Decode and validate cookie
+    def decode_cookie(self, cookie: bytes) -> typing.Dict[str, typing.Any]:
+        result: typing.Dict[str, typing.Any] = ModifiedDict()
+        try:
+            data = self.signer.unsign(
+                cookie, max_age=self.max_age, return_timestamp=True
+            )
+            result = ModifiedDict(json.loads(b64decode(data[0])))
+        except (BadSignature, SignatureExpired):
+            result.invalid = True
+            return result
+        
+        #data[1] is the datetime when signed from itsdangerous
+        expiration = data[1] + timedelta(seconds=self.max_age)  # type: ignore[arg-type]
+        if self.refresh_window:
+            now = datetime.now(timezone.utc)
+
+            # The cookie is with in the refresh window, trigger a refresh.
+            if now >= (expiration - timedelta(seconds=self.refresh_window)) and now <= expiration:  # noqa E501
+                result.modify = True
+        return result
 
     async def __call__(self, scope: Scope, receive: Receive, send: Send) -> None:
         if scope["type"] not in ("http", "websocket"):  # pragma: no cover
@@ -39,45 +102,41 @@ class SessionMiddleware:
             return
 
         connection = HTTPConnection(scope)
-        initial_session_was_empty = True
-
+        
         if self.session_cookie in connection.cookies:
-            data = connection.cookies[self.session_cookie].encode("utf-8")
-            try:
-                data = self.signer.unsign(data, max_age=self.max_age)
-                scope["session"] = json.loads(b64decode(data))
-                initial_session_was_empty = False
-            except BadSignature:
-                scope["session"] = {}
+            scope["session"] = self.decode_cookie(
+                connection.cookies[self.session_cookie].encode("utf-8")                
+            )  # noqa E501
         else:
-            scope["session"] = {}
+            scope["session"] = ModifiedDict()
 
         async def send_wrapper(message: Message) -> None:
             if message["type"] == "http.response.start":
-                if scope["session"]:
-                    # We have session data to persist.
-                    data = b64encode(json.dumps(scope["session"]).encode("utf-8"))
-                    data = self.signer.sign(data)
+                if scope["session"] and not scope["session"].invalid: #Scope has session data and is valid.
+                    if scope["session"].modify: #Scope has updated data or needs refreshing.
+                        data = b64encode(json.dumps(scope["session"]).encode("utf-8"))
+                        data = self.signer.sign(data)
+                        print (datetime.now(timezone.utc))
+                        headers = MutableHeaders(scope=message)
+                        header_value = "{session_cookie}={data}; path={path}; {max_age}{security_flags}".format(  # noqa E501
+                            session_cookie=self.session_cookie,
+                            data=data.decode("utf-8"),
+                            path=self.path,
+                            max_age=f"Max-Age={self.max_age}; " if self.max_age else "",
+                            security_flags=self.security_flags,
+                        )
+                        headers.append("Set-Cookie", header_value)
+                #if the session cookie is invalid for any reason, it needs to be cleared.
+                elif scope['session'].invalid: #Clear the cookie.
                     headers = MutableHeaders(scope=message)
                     header_value = "{session_cookie}={data}; path={path}; {max_age}{security_flags}".format(  # noqa E501
                         session_cookie=self.session_cookie,
-                        data=data.decode("utf-8"),
-                        path=self.path,
-                        max_age=f"Max-Age={self.max_age}; " if self.max_age else "",
-                        security_flags=self.security_flags,
-                    )
-                    headers.append("Set-Cookie", header_value)
-                elif not initial_session_was_empty:
-                    # The session has been cleared.
-                    headers = MutableHeaders(scope=message)
-                    header_value = "{session_cookie}={data}; path={path}; {expires}{security_flags}".format(  # noqa E501
-                        session_cookie=self.session_cookie,
                         data="null",
                         path=self.path,
-                        expires="expires=Thu, 01 Jan 1970 00:00:00 GMT; ",
+                        max_age="Max-Age=-1; ",
                         security_flags=self.security_flags,
                     )
                     headers.append("Set-Cookie", header_value)
+                #No session cookie was present, or it isn't modified, don't modify or delete the cookie.
             await send(message)
-
         await self.app(scope, receive, send_wrapper)
